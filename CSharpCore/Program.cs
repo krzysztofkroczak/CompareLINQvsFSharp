9bb35a39b5d8917af9195bfcd8a2d90fd0600627
Krzysztof Kroczak <krzysztof.kroczak@red-gate.com>
2019-01-01 01:09:59 +0000

    Comparison

---
+++ CSharpCore/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace CSharpCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = new Stopwatch();
+            Console.WriteLine("first execution");
+            stopwatch.Start();
+            var a1 = Test();
+            stopwatch.Stop();
+            WriteToFile(1, a1);
+            Console.WriteLine($"time: {stopwatch.Elapsed}");
+            stopwatch.Reset();
+            Console.WriteLine("second execution");
+            stopwatch.Start();
+            var a2 = Test();
+            stopwatch.Stop();
+            WriteToFile(2, a2);
+            Console.WriteLine($"time: {stopwatch.Elapsed}");
+            Console.ReadLine();
+        }
+
+        private static void WriteToFile(int i, string a1)
+        {
+            using (var x = new StreamWriter(
+                new FileStream($"C:\\SomePath\c{i}.txt", FileMode.Append)))
+            {
+                x.Write(a1);
+            }
+        }
+
+        private static string Test()
+        {
+            return string.Concat(Enumerable.Range(0, 500_001)
+                .Where(x => x % 5 == 0)
+                .Select(x => x * 2)
+                .Select(x => x.ToString()));
+        }
+    }
+}
---
+++ CheckInFSharpConsole/Program.fs
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+open System.Diagnostics
+open System.IO
+
+let test () =
+    [0..500_000] 
+    |> List.filter (fun x -> x % 5  = 0)
+    |> List.map (fun x -> x * 2)
+    |> List.map (fun x -> x.ToString())
+    |> String.concat "+"
+
+let writeFile i (result: String) = 
+    use stream = new StreamWriter(
+    new FileStream(sprintf "C:\\Users\\Krzysztof.Kroczak\\source\\repos\\CheckTransformations\\CheckTransformations\\a%d.txt" i,FileMode.Append))
+    stream.Write(result);
+
+[<EntryPoint>]
+let main argv = 
+    let stopWatch = new Stopwatch();
+
+    printfn "First execution"
+    stopWatch.Start()
+    let result1 = test()
+    stopWatch.Stop()
+    writeFile 1 result1
+    printfn "result %A" stopWatch.Elapsed
+    stopWatch.Reset()
+
+    printfn "Second execution"
+    stopWatch.Start()
+    let result2 = test()
+    stopWatch.Stop()
+    writeFile 2 result2
+    printfn "result %A" stopWatch.Elapsed
+
+    0
